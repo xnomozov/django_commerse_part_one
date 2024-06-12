6ec41f7afbc43bad92dee397df8f51d2c2cac7d4
xakimjon <xakimjonnomozov8@gmail.com>
2024-06-12 13:41:58 +0500

    part 2 done

--- app/models.py
+++ app/models.py
@@ -20,7 +20,44 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+    def get_attribute(self):
+        product_attribute = ProductAttribute.objects.filter(product=self)
+        attributes = []
+        for prod_at in product_attribute:
+            attributes.append({
+                'attribute_key': prod_at.key,
+                'attribute_value': prod_at.value
+            })
+        return attributes
+
+    def attributes_as_dict(self):
+        attributes = self.get_attribute()
+        attributes_dict = {}
+        for attr in attributes:
+            attributes_dict[attr['attribute_key']] = attr['attribute_value']
+        return attributes_dict
+
 
 class Images(models.Model):
     image = models.ImageField(upload_to='images', null=True)
     product = models.ForeignKey('app.Product', on_delete=models.CASCADE, related_name='images')
+
+
+class AttributeKey(models.Model):
+    key = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.key
+
+
+class AttributeValue(models.Model):
+    value = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.value
+
+
+class ProductAttribute(models.Model):
+    product = models.ForeignKey('app.Product', on_delete=models.CASCADE)
+    key = models.ForeignKey('app.AttributeKey', on_delete=models.CASCADE)
+    value = models.ForeignKey('app.AttributeValue', on_delete=models.CASCADE)
--- app/views.py
+++ app/views.py
@@ -6,7 +6,21 @@ from app.models import Product
 # Create your views here.
 def index(request):
     products = Product.objects.all()
-    context = {'products': products,
-               }
+    context = {'products': products}
 
+    return render(request, 'app/index.html', context)
+
+
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    attributes = product.get_attribute()
+    context = {'product': product,
+               'attributes': attributes}
+    return render(request, 'app/product-details.html', context)
+
+
+def prod_attr(request, product_id):
+    product = Product.objects.get(id=product_id)
+    attributes = product.attributes_to_list()
+    context = {'attributes': attributes, }
     return render(request, 'app/index.html', context)
