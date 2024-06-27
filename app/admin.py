03cb592a3cff8f2a0506cda538b9b29ea825c4b5
Vohidjon <vohidjonboyqoziyev@yahoo.com>
2024-06-28 02:37:26 +0500

    Registration done, adminka changed

--- app/admin.py
+++ app/admin.py
@@ -9,3 +9,22 @@ admin.site.register(AttributeKey)
 admin.site.register(AttributeValue)
 admin.site.register(ProductAttribute)
 admin.site.register(Customers)
+
+@admin.register(Customers)
+class CustomerModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name','email','phone','billing_address', 'joined']
+    search_fields = ['name', 'email', 'id']
+    list_filter = ['joined', 'is_active']
+
+
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'price', 'rating', 'quantity']
+    search_fields = ['name', 'price', 'rating']
+    list_filter = ['price', 'rating']
+
+
+@admin.register(Images)
+class ImagesModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'image', 'product']
+    search_fields = ['product']
